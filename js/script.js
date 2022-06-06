9e7a86c4b52bd7917f2f1cd67d3fbf1d60c9a84f
Dag-da <106157620+Dag-da@users.noreply.github.com>
2022-06-06 09:46:20 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let x = document.getElementById("menu")
+
+function mobileBar() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+
+    } else {
+        menu.style.display = "none";
+    }
+}
